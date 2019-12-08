7121200c1e16a66cec95868159f600d752a7cc94
zqlimy17 <zqlimy@gmail.com>
2019-12-08 14:24:57 +0800

    cleaned up server.js, installed passport

--- server.js
+++ server.js
@@ -3,26 +3,31 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const app = express();
+require("dotenv").config();
 
-// Environment Variables (getting ready for Heroku)
-const PORT = process.env.PORT || 5000;
+// Environment Variables
+const PORT = process.env.PORT;
+
+// Global Configuration
+const db = mongoose.connection;
+
+// Mongoose Deprecation Warnings
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
 // Middleware
-const usersControllers = require("./controllers/users");
-app.use("/users", usersControllers);
-app.use(express.json()); //use .json(), not .urlencoded()
+app.use(express.json());
 app.use(express.static("public"));
 
 app.use(
   session({
-    secret: "D1D18649F8AB8807F0868CCEED680F547C2A233AFBDF99686ACC12297150330E", //a random string do not copy this value or your stuff will get hacked
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false
   })
 );
-
-// Routes
-const db = mongoose.connection;
 
 // Environment Variables
 const mongoURI =
@@ -33,14 +38,20 @@ mongoose.connect(mongoURI, { useNewUrlParser: true }, () =>
   console.log("MongoDB connection established:", mongoURI)
 );
 
-// Error / Disconnection
-db.on("error", err => console.log(err.message + " is Mongod not running?"));
+// Connection Error/Success
+db.on("error", err => console.log(err.message + " is mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", mongoURI));
 db.on("disconnected", () => console.log("mongo disconnected"));
 
+// Controllers
+const usersControllers = require("./controllers/users");
+app.use("/users", usersControllers);
+
+// Routes
 app.get("/*", (req, res) => {
   res.redirect("/");
 });
 
 app.listen(PORT, () => {
-  console.log("Let's get things done on port", PORT);
+  console.log("Watching Movies on Port", PORT);
 });
