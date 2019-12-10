5e59ad5c1566ddf48dc00f0203509cf0c90e4e85
Pei Xuan Ong <opx@Peis-MacBook-Pro.local>
2019-12-10 21:59:45 +0800

    Update search movie component

--- public/js/components/MoviesResult.js
+++ public/js/components/MoviesResult.js
@@ -1,62 +1,25 @@
 class MoviesResult extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      baseURL: "https://api.themoviedb.org/3/",
-      apikey: "api_key=" + "1a31cfdf9cc81f7229bbbc09db5d95bd",
-      query: "&query=",
-      searchquery: "search/movie?",
-      movieTitle: "Jack+Reacher",
-      searchURL: "",
-      movieResults: []
-    };
-  }
-  componentDidMount() {
-    this.setState(
-      {
-        searchURL:
-          this.state.baseURL +
-          this.state.searchquery +
-          this.state.apikey +
-          this.state.query +
-          this.state.movieTitle
-      },
-      () => {
-        console.log("search url:" + this.state.searchURL);
-        console.log("base url:" + this.state.baseURL);
-        fetch(this.state.searchURL)
-          .then(response => {
-            return response.json();
-          })
-          .then(
-            json => {
-              this.setState({
-                movieResults: json.results
-              });
-              console.log(json.results);
-            },
-            err => console.log(err)
-          );
-      }
-    );
-  }
   render() {
     return (
-      <div className="movies-result">
+      <div className='movies-result'>
         <h1>Movie Result Goes Here</h1>
         <ul>
-          {this.state.movieResults.map(movie => {
-            return (
-              <React.Fragment>
-                <ul>
-                  <li> {movie.overview}</li>
-                  <li> {movie.title}</li>
-                  <li> {movie.release_date}</li>
-                  <li> {movie.poster_path}</li>
-                </ul>
-              </React.Fragment>
-            );
-          })}
+          {this.props.movieResults ? (
+            this.props.movieResults.map(movie => {
+              return (
+                <React.Fragment>
+                  <ul>
+                    <li> {movie.overview}</li>
+                    <li> {movie.title}</li>
+                    <li> {movie.release_date}</li>
+                    <li> {movie.poster_path}</li>
+                  </ul>
+                </React.Fragment>
+              );
+            })
+          ) : (
+            <div>Search Movie Results</div>
+          )}
         </ul>
       </div>
     );
--- public/js/components/Searchbar.js
+++ public/js/components/Searchbar.js
@@ -1,9 +1,80 @@
-class Searchbar extends React.Component {
+class SearchBar extends React.Component {
   render() {
     return (
-      <div className="search-bar">
-        <h2>Search bar goes here</h2>
-      </div>
+      <React.Fragment>
+        <div className='search-bar'>
+          <form>
+            <input
+              type='text'
+              name='searchbar'
+              id='searchbar'
+              placeholder='Search Movies...'
+            ></input>
+            <input class='btn btn-primary' type='submit' value='Search'></input>
+          </form>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      baseURL: 'https://api.themoviedb.org/3/',
+      apikey: 'api_key=' + '1a31cfdf9cc81f7229bbbc09db5d95bd',
+      query: '&query=',
+      searchquery: 'search/movie?',
+      movieTitle: 'Jack+Reacher',
+      searchURL: '',
+      movieResults: []
+    };
+  }
+  componentDidMount() {
+    this.setState(
+      {
+        searchURL:
+          this.state.baseURL +
+          this.state.searchquery +
+          this.state.apikey +
+          this.state.query +
+          this.state.movieTitle
+      },
+      () => {
+        console.log('search url:' + this.state.searchURL);
+        console.log('base url:' + this.state.baseURL);
+        fetch(this.state.searchURL)
+          .then(response => {
+            return response.json();
+          })
+          .then(
+            json => {
+              this.setState({
+                movieResults: json.results
+              });
+              console.log(json.results);
+            },
+            err => console.log(err)
+          );
+      }
+    );
+  }
+  seachMovie = event => {
+    let text = event.target.innerText;
+    this.setState({
+      movieTitle: text
+    });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.movieResults ? (
+          <MoviesResult movieResults={this.state.movieResults} />
+        ) : (
+          ''
+        )}
+      </React.Fragment>
     );
   }
 }
